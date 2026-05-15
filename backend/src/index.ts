5c364d76349a9a718cbc9e2edd38a02d3dcbffe3
abcd <abcd@example.com>
2026-05-15 01:16:42 -0700

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,19 @@ import { connectDB } from './config/database';
 import { createServer } from 'http';
 import { initSocket } from './socket';
 
-const PORT = process.env.PORT || 5000;
-
 const httpServer = createServer(app);
 initSocket(httpServer);
 
-connectDB().then(() => {
-    httpServer.listen(PORT, () => {
-        console.log(`Server is runningjnhjhjh on port ${PORT}`);
-    });
+// Connect to Database
+connectDB().catch(err => {
+    console.error('Initial DB connection failed:', err);
 });
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    httpServer.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
